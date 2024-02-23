e9f371f4cc1b41c2ce44b4fade171b9b0d7fcbdf
GitHubBruger <ianb@kea.dk>
2024-02-23 15:10:54 +0100

    Initial commit

---
+++ src/main/java/kea/iabr/hellogithubactionsspringboot101/HelloGitHubActionsSpringBoot101Application.java
@@ -0,0 +1,13 @@
+package kea.iabr.hellogithubactionsspringboot101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloGitHubActionsSpringBoot101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloGitHubActionsSpringBoot101Application.class, args);
+	}
+
+}
---
+++ src/test/java/kea/iabr/hellogithubactionsspringboot101/HelloGitHubActionsSpringBoot101ApplicationTests.java
@@ -0,0 +1,13 @@
+package kea.iabr.hellogithubactionsspringboot101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloGitHubActionsSpringBoot101ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

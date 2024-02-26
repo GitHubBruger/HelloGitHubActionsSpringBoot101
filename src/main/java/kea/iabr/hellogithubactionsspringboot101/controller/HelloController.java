fcc67fd4c18bda980c0ee9a0ab250695a85ab4cf
GitHubBruger <ianb@kea.dk>
2024-02-26 12:34:32 +0100

    Added test

--- src/main/java/kea/iabr/hellogithubactionsspringboot101/controller/HelloController.java
+++ src/main/java/kea/iabr/hellogithubactionsspringboot101/controller/HelloController.java
@@ -4,12 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(path = "hello")
 public class HelloController {
 
     @RequestMapping("")
     public String index() {
-        return "greeting";
+        return "welcome";
     }
 
 }
